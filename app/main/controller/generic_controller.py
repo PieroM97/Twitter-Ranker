36d47c229b421a423020ec548a5fca57b982c309
PieroM97 <pierom97@gmail.com>
2023-04-20 11:17:10 +0200

    Revert "Revert "innit""
    This reverts commit ad9d21f2cd2dc22acb30f750d246199071faaa4a.

--- app/main/controller/generic_controller.py
+++ app/main/controller/generic_controller.py
@@ -1,26 +1,62 @@
-from flask import Blueprint,render_template,request
-import app.main.service.sentiment_service as sentiment
+from flask import Blueprint, request
+
+
+from app.main.service.crawler import *
+from app.main.service.deep_learning import *
+from app.main.service.elastic import *
 
 
 
-Controller_blueprint = Blueprint("QAController_blueprint", __name__)
-
-@Controller_blueprint.route("/")
-def index():
-    return render_template('index.html')
-
-@Controller_blueprint.post("/")
-def getResult():
-    username = request.form['username-text']
-    result = sentiment.get_user_score(username, 20, "hugging")
-    return render_template("result.html", result=result)
+from trafilatura.spider import focused_crawler
 
 
-@Controller_blueprint.app_errorhandler(500)
-def error():
-    return render_template("error.html"),500
+Controller_blueprint = Blueprint("Controller_blueprint", __name__)
 
-@Controller_blueprint.app_errorhandler(404)
-def not_found(error):
-    return render_template("error.html"),404
+#Indexation
 
+@Controller_blueprint.post("/crawl/summerize")
+def postSummerize():
+
+  url = request.get_json()['url']
+  content = getSinglePage(url)
+
+  return summarization_task(content),200
+
+@Controller_blueprint.post("/crawl/embedding")
+def postEmbedding():
+
+  url = request.get_json()['url']
+
+  content = getSinglePage(url)
+  embedding = text_embedding(content)
+
+  result = index_document(content,embedding)
+
+  return result,200
+
+
+#Search
+
+@Controller_blueprint.post("/vector_search")
+def postSearch():
+
+  query = request.get_json()['query']
+  vector_query =  text_embedding(query)
+  return index_search(vector_query).body,200
+
+
+@Controller_blueprint.post("/question_answering")
+def postQuestion():
+
+  query = request.get_json()['query']
+  vector_query =  text_embedding(query)
+  answer = question_answering(index_search(vector_query).body,query)
+  return answer,200
+
+
+@Controller_blueprint.post("/chatbot")
+def postChatbot():
+    query = request.get_json()['query']
+    vector_query = text_embedding(query)
+    answer = chat_answering(index_search(vector_query).body, query)
+    return answer, 200
